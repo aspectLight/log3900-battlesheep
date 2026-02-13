b3cc1d689d97b743f5b303544c256607e6f9fe60
Pierre Carré <pierrecarre05@gmail.com>
2026-02-13 13:38:40 -0500

    fix: suppression des fonctions ping de debug des controllers

--- server/app/modules/game/controllers/game.controller.ts
+++ server/app/modules/game/controllers/game.controller.ts
@@ -13,9 +13,6 @@ export class GameController {
         private readonly gameValidationService: GameValidationService,
     ) {}
 
-    @Get('ping')
-    ping() { return "pong"; }
-
     @Get()
     async findAllGames(@Res() response: Response) {
         try {
