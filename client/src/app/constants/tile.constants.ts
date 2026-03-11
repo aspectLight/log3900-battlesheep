03ec4e45b3931ecfaee2efe067047133e7e25e9b
Anis13200 <anosgadouche@gmail.com>
2026-03-11 17:10:53 -0400

    feat(tiles): mocks des tuiles supprimés

--- client/src/app/constants/tile.constants.ts
+++ client/src/app/constants/tile.constants.ts
@@ -246,26 +246,6 @@ export const TILE_TYPES: {
         defaultOrientation: 'TUp',
         rotations: ['TUp', 'TRight', 'TDown', 'TLeft', 'Cross'],
     },
-    trap: {
-        name: 'Tree',
-        description: 'Un arbre qui ne peut pas être grimpé',
-        baseMoveModifier: -1,
-        images: {
-            default: './assets/tiles/tree.png',
-        },
-        defaultState: 'default',
-        defaultOrientation: '',
-    },
-    teleportPad: {
-        name: 'Tree',
-        description: 'Un arbre qui ne peut pas être grimpé',
-        baseMoveModifier: -1,
-        images: {
-            default: './assets/tiles/tree.png',
-        },
-        defaultState: 'default',
-        defaultOrientation: '',
-    },
 };
 
 export const ALLOWED_TILES = ['door', 'water', 'ice', 'wall', 'tree', 'stone', 'trap'];
