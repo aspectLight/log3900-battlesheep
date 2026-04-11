fbaeb584a14c7dbc675f7f72bc21399f1e9f59f7
Pierre Carré <pierrecarre05@gmail.com>
2026-04-10 22:08:45 -0400

    fix: génération des routes

--- client_leger/lib/routing/app_router.gr.dart
+++ client_leger/lib/routing/app_router.gr.dart
@@ -59,6 +59,22 @@ class CharacterCreationRoute extends PageRouteInfo<void> {
 }
 
 /// generated route for
+/// [DiscussionCanalsScreen]
+class DiscussionCanalsRoute extends PageRouteInfo<void> {
+  const DiscussionCanalsRoute({List<PageRouteInfo>? children})
+    : super(DiscussionCanalsRoute.name, initialChildren: children);
+
+  static const String name = 'DiscussionCanalsRoute';
+
+  static PageInfo page = PageInfo(
+    name,
+    builder: (data) {
+      return const DiscussionCanalsScreen();
+    },
+  );
+}
+
+/// generated route for
 /// [FriendsScreen]
 class FriendsRoute extends PageRouteInfo<void> {
   const FriendsRoute({List<PageRouteInfo>? children})
