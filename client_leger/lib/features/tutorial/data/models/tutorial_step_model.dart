1adb3d465d2b67f944e88fd0dbd512ffa8402746
Pierre Carré <pierrecarre05@gmail.com>
2026-04-15 16:22:40 -0400

    fix(tutoriel): Changement des images du tutoriel

--- client_leger/lib/features/tutorial/data/models/tutorial_step_model.dart
+++ client_leger/lib/features/tutorial/data/models/tutorial_step_model.dart
@@ -14,36 +14,36 @@ const kTutorialSteps = [
   TutorialStepModel(
     titleKey: 'profileTitle',
     descriptionKey: 'profileDesc',
-    image: 'assets/images/tutorial/background.png',
+    image: 'assets/images/tutorial/profile.png',
   ),
   TutorialStepModel(
     titleKey: 'friendsTitle',
     descriptionKey: 'friendsDesc',
-    image: 'assets/images/tutorial/background.png',
+    image: 'assets/images/tutorial/friends.png',
   ),
   TutorialStepModel(
     titleKey: 'chatTitle',
     descriptionKey: 'chatDesc',
-    image: 'assets/images/tutorial/background.png',
+    image: 'assets/images/tutorial/chat.png',
   ),
   TutorialStepModel(
     titleKey: 'shopTitle',
     descriptionKey: 'shopDesc',
-    image: 'assets/images/tutorial/background.png',
+    image: 'assets/images/tutorial/shop.png',
   ),
   TutorialStepModel(
     titleKey: 'gameModesTitle',
     descriptionKey: 'gameModesDesc',
-    image: 'assets/images/tutorial/background.png',
+    image: 'assets/images/tutorial/game_modes.png',
   ),
   TutorialStepModel(
     titleKey: 'createGameTitle',
     descriptionKey: 'createGameDesc',
-    image: 'assets/images/tutorial/background.png',
+    image: 'assets/images/tutorial/create_game.png',
   ),
   TutorialStepModel(
     titleKey: 'joinGameTitle',
     descriptionKey: 'joinGameDesc',
-    image: 'assets/images/tutorial/background.png',
+    image: 'assets/images/tutorial/join_game.png',
   ),
 ];
